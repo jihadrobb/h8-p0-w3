221cb91a9cbd5419ea2606f05a64b797b43c47ef
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-03-31 14:59:06 +0700

    join

--- exercise2.js
+++ exercise2.js
@@ -1,15 +1,11 @@
 // Tantangan Array 1
-var kata = 'test';
+var kata = 'dicoba coba';
 
 function balikString(kata){
     var dibalik = [];
     for(var i=kata.length-1; i>=0; i--){
         dibalik.push(kata[i]);
     }
-    var ini = [];
-    for(var j=0; j<dibalik.length; j++){
-        ini.concat(dibalik[i]);
-    }
-    return ini;
+    return dibalik.join('');
 }
 console.log(balikString(kata));
