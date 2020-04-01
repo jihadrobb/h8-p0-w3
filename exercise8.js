d18471e0053dc8f1d013aca4716a9789b9c3374b
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-04-01 13:54:35 +0700

    beautify

--- exercise8.js
+++ exercise8.js
@@ -1,19 +1,19 @@
 //Exercise 8 Pasangkan Angka Terbesar
 function pasanganTerbesar(num) {
     var str = num.toString();
-    var max = parseInt(str[0]+str[1]);
-    for(var i=1; i<str.length-1; i++){
-        var temp = parseInt(str[i] + str[i+1]);
-        if(temp > max){
+    var max = parseInt(str[0] + str[1]);
+    for (var i = 1; i < str.length - 1; i++) {
+        var temp = parseInt(str[i] + str[i + 1]);
+        if (temp > max) {
             max = temp;
         }
     }
     return max;
-  }
-  
-  // TEST CASES
-  console.log(pasanganTerbesar(641573)); // 73
-  console.log(pasanganTerbesar(12783456)); // 83
-  console.log(pasanganTerbesar(910233)); // 91
-  console.log(pasanganTerbesar(71856421)); // 85
-  console.log(pasanganTerbesar(79918293)); // 99
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
