0f15ee6616451a4cff50df2637e12cf07eea0391
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-04-03 12:49:26 +0700

    Update exercise15.js

--- exercise15.js
+++ exercise15.js
@@ -38,4 +38,3 @@ function groupAnimals(animals) {
   // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
   // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
-  console.log('a'<'b');
